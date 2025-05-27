9383df7581728041c6939e8b2f64160dfa12d19d
warwoyo <bobobobi.krn@gmail.com>
2025-05-27 15:17:17 +0700

    add : update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
         </head>
         <body>
             <div class="login-form">
-                <h2>Login</h2>
+                <h2>Login new 2025</h2>
                 <form action="/login" method="POST">
                     <input type="text" name="username" placeholder="Username" required>
                     <input type="password" name="password" placeholder="Password" required>
